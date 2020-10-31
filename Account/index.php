cdd12d54617538284679b2ae8b0dd5fe756e31e6
liuzhiheng <liuzhiheng@sensetime.com>
2020-10-31 22:46:33 +0800

    fix: style fix and session fix in detail page

--- Account/index.php
+++ Account/index.php
@@ -25,9 +25,7 @@
         $dbpass = "f38ee";
         $dbname = "f38ee";
 
-        // Create connection
         $conn = mysqli_connect($servername, $dbuser, $dbpass, $dbname);
-        // Check connection
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
